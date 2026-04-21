40231d9a5f8fe3411553e9a41a61688d9bebf02d
ShukaOriginal <kuraga908@mail.ru>
2026-04-21 13:07:28 +0700

    added game progresion in snake game

--- src/games/test.js
+++ src/games/test.js
@@ -1,7 +1,6 @@
 import readline from 'node:readline'
-import { randomNumber, chars } from '../index.js'
+import { randomNumber, randomChars } from '../index.js'
 import chalk from 'chalk'
-import { cp } from 'node:fs'
 
 readline.emitKeypressEvents(process.stdin)
 if (process.stdin.isTTY) {
@@ -11,12 +10,6 @@ if (process.stdin.isTTY) {
 // size of the game field, where are: height = quantity of rows in the array, width: length of rows
 let width = 9
 let height = 9
-
-const randomChars = () => {
-  const symbols = chars
-  const randomChar = symbols[randomNumber(symbols.length)]
-  return randomChar
-}
 
 const snake = [
   { x: 1, y: 1 },
@@ -52,31 +45,25 @@ function generateSnakeOnField(field, snake) {
 const makeAStep = () => {
   console.clear()
 
+  playerLengthProgress(snake)
+
   const step = {
     x: snake.at(-1).x + direction.x,
     y: snake.at(-1).y + direction.y,
   }
-  const [field] = fieldGenerator(width, height, '.')
-  havePlayerBitedHimself(snake, step)
-  checkIfPlayerInTheArea(field, step)
-  showCollecteables(field,collectables)
 
-  if(collectables.apple.x === step.x && collectables.apple.y === step.y){
-    snake.push(step)
-    collectables.apple = generateThing(snake)
-  }
-  else if(collectables.resize.x === step.x && collectables.resize.y === step.y){
-    width += 1
-    height += 1
-    collectables.resize = generateThing(snake)
-    snake.push(step)
-    snake.shift()
-  }
-  else{
-    snake.push(step)
-    snake.shift()
-  }
+  const [field] = fieldGenerator(width, height, '.')
+
+  showCollecteables(field, collectables)
+  checkIfPlayerInTheArea(field, step)
+  havePlayerBitedHimself(snake, step)
+  playerEating(step, collectables)
+
+  snake.push(step)
+  snake.shift()
+
   const resultedField = generateSnakeOnField(field, snake)
+
   console.log(resultedField.map(array => array.join(' ')).join('\n'))
 }
 
@@ -105,7 +92,7 @@ function havePlayerBitedHimself(snake, newStep) {
   for (const { x, y } of snake) {
     if (x === newStep.x && y === newStep.y) {
       console.log(
-        'You just bited yourself! I guess apples aren\'t that good huh ?',
+        'You just bited yourself! I guess apples aren\'t that good ?',
       )
       process.exit()
     }
@@ -137,27 +124,67 @@ function generateThing(snake) {
   return thing
 }
 
-setInterval(makeAStep, 400)
-
-export default () => {
-  return makeAStep()
-}
-
-// const collectable = {
-//   apple: { x: randomNumber(width), y: randomNumber(height) },
-// }
-
-function showCollecteables(field,collectables){
+function showCollecteables(field, collectables) {
   const things = {
     apple: chalk.red('@'),
-    resize: chalk.cyan('⇆')
+    resize: chalk.blue('⇆'),
   }
   const entries = Object.entries(collectables)
-  for(const [name,coordinates] of entries){
+  for (const [name, coordinates] of entries) {
     field[coordinates.y][coordinates.x] = things[name]
   }
   return field
 }
 
+function playerEating(step, collectables) {
+  const entries = Object.entries(collectables)
+  for (const [name, coords] of entries) {
+    if (step.x === coords.x && step.y === coords.y) {
+      switch (name) {
+        case 'apple':
+          snake.push(step)
+          collectables.apple = generateThing(snake)
+          break
+        case 'resize':
+          width += 1
+          height += 1
+          collectables.resize = generateThing(snake)
+          break
+        default:
+          break
+      }
+    }
+  }
+}
 
+function playerLengthProgress(snake) {
+  const targetScore = 50
+  const snakeLength = snake.length - 3
+  if (snakeLength === 10) {
+    console.log('You doing great! Keep going.')
+  }
+  else if (snakeLength === 20) {
+    console.log(
+      `Alredy ${snakeLength}! ${targetScore - snakeLength} left!`,
+    )
+  }
+  else if (snakeLength === 30) {
+    console.log(`You getting closer, ${targetScore - snakeLength} left!`)
+  }
+  else if (snakeLength === 40) {
+    console.log(`Only ${targetScore - snakeLength} left! Cmon do it!`)
+  }
+  else if (snakeLength === 50) {
+    process.exit()
+    console.log(`You ate ${snakeLength} of 50 apples! Great You won!`)
+  }
+  else {
+    console.log(`You ate: ${snakeLength} apples.`)
+  }
+}
 
+setInterval(makeAStep, 300)
+
+export default () => {
+  return makeAStep()
+}
--- src/index.js
+++ src/index.js
@@ -2,53 +2,11 @@ export const randomNumber = (range = 10) => {
   return Math.floor(Math.random() * range)
 }
 
-export const chars = [
-  't',
-  'V',
-  'r',
-  '<',
-  '#',
-  'A',
-  'H',
-  'L',
-  '[',
-  'r',
-  'e',
-  'H',
-  'j',
-  'q',
-  '{',
-  'z',
-  'c',
-  'm',
-  'P',
-  '~',
-  'W',
-  '<',
-  'u',
-  '!',
-  '&',
-  '7',
-  'u',
-  'L',
-  'S',
-  'i',
-  'E',
-  'h',
-  '#',
-  'T',
-  'v',
-  'y',
-  '!',
-  'l',
-  '&',
-  'O',
-  '+',
-  'Z',
-  'v',
-  'Q',
-  'Q',
-  'w',
-  '8',
-  '$',
-]
+export const randomChars = () => {
+  const chars = [
+    't', 'V', 'r', '<', '#', 'A', 'H', 'L', '[', 'r', 'e', 'H', 'j', 'q', '{', 'z', 'c', 'm', 'P', '~', 'W', '<', 'u',
+    '!', '&', '7', 'u', 'L', 'S', 'i', 'E', 'h', '#', 'T', 'v', 'y', '!', 'l', '&', 'O', '+', 'Z', 'v', 'Q', 'Q', 'w', '8', '$',
+  ]
+  const randomChar = chars[randomNumber(chars.length)]
+  return randomChar
+}
