2cee7674ad1427781ed611b81ef5e013783c0acb
Bron <bro0612@mail.ru>
2026-04-21 19:11:45 +0700

    Snake game updated, added progression

--- src/games/snake.js
+++ src/games/snake.js
@@ -1,5 +1,5 @@
 import readline from 'node:readline'
-import { randomNumber, chars } from '../index.js'
+import { randomNumber, randomChars } from '../index.js'
 import chalk from 'chalk'
 
 readline.emitKeypressEvents(process.stdin)
@@ -7,7 +7,7 @@ if (process.stdin.isTTY) {
   process.stdin.setRawMode(true)
 }
 
-// size of the game field, where are: height = qantity of row in the main array, and width: length of the rows
+// size of the game field, where are: height = quantity of rows in the array, width: length of rows
 let width = 9
 let height = 9
 
@@ -17,65 +17,57 @@ const snake = [
   { x: 3, y: 1 },
 ]
 
-// generates one of many diffent chars, fieldGenerator uses this fucntion to represent the snake on the field
-export const randomChars = () => {
-  const symbols = chars
-  const randomChar = symbols[randomNumber(symbols.length)]
-  return randomChar
+const collectables = {
+  apple: generateThing(snake),
+  resize: generateThing(snake),
 }
 
-// game objects
-let resizeArea = { x: randomNumber(width), y: randomNumber(height) }
-let apple = { x: randomNumber(width), y: randomNumber(height) }
-// snake's next move direction, using it in the makeAStep funtion.
-let direction = { x: 0, y: 1 }
+// Snake next move direction, using it in the makeAStep funtion.
+let direction = { x: 1, y: 0 }
 
+// Field generation, returns an array of array
 function fieldGenerator(width, heigth, background) {
   const field = []
   for (let i = 0; i < heigth; i++) {
     field.push(Array(width).fill(background))
   }
-  for (const { x, y } of snake) {
-    field[y][x] = chalk.green(randomChars())
-  }
-  field[resizeArea.y][resizeArea.x] = chalk.blue('⇄')
-  field[apple.y][apple.x] = chalk.red('@')
   return [field, width, heigth]
 }
 
-// let direction = { x: 0, y: 1 }
+// Generates snake on the field, returns field with snake on it
+function generateSnakeOnField(field, snake) {
+  for (const { x, y } of snake) {
+    field[y][x] = chalk.green(randomChars())
+  }
+  return field
+}
 
-// const snake = [
-//   { x: 1, y: 1 },
-//   { x: 2, y: 1 },
-//   { x: 3, y: 1 },
-// ]
 const makeAStep = () => {
   console.clear()
+
+  playerLengthProgress(snake)
+
   const step = {
     x: snake.at(-1).x + direction.x,
     y: snake.at(-1).y + direction.y,
   }
-  isPlayerStillAlive(snake, step)
-  if (step.x === apple.x && step.y === apple.y) {
-    snake.push(step)
-    apple = generateThing(snake)
-  }
-  else if (step.x === resizeArea.x && step.y === resizeArea.y) {
-    width += 1
-    height += 1
-    resizeArea = generateThing(snake)
-  }
-  else {
-    snake.push(step)
-    snake.shift()
-  }
+
   const [field] = fieldGenerator(width, height, '.')
-  checkIfPlayerInTheArea(step, field)
-  console.log(field.map(array => array.join(' ')).join('\n'))
+
+  showCollecteables(field, collectables)
+  checkIfPlayerInTheArea(field, step)
+  havePlayerBitedHimself(snake, step)
+  playerEating(step, collectables)
+
+  snake.push(step)
+  snake.shift()
+
+  const resultedField = generateSnakeOnField(field, snake)
+
+  console.log(resultedField.map(array => array.join(' ')).join('\n'))
 }
 
-// snake movement ←↑↓→
+// snake controle buttons ←↑↓→
 process.stdin.on('keypress', (_, key) => {
   if (key.ctrl && key.name === 'c') process.exit()
 
@@ -95,7 +87,28 @@ process.stdin.on('keypress', (_, key) => {
   }
 })
 
-// Apple generation
+// Checking if player next step was made over snakes body
+function havePlayerBitedHimself(snake, newStep) {
+  for (const { x, y } of snake) {
+    if (x === newStep.x && y === newStep.y) {
+      console.log(
+        'You just bited yourself! I guess apples aren\'t that good ?',
+      )
+      process.exit()
+    }
+  }
+}
+// This function checks if player exist in the playing area
+function checkIfPlayerInTheArea(field, step) {
+  const { x, y } = step
+  if (field[y] === undefined || field[y][x] === undefined) {
+    console.log(
+      'Looks like you got out of playing area!\nThe game`ll be restarted',
+    )
+    return process.exit()
+  }
+}
+
 function generateThing(snake) {
   let isCollsion = true
   let thing
@@ -111,38 +124,54 @@ function generateThing(snake) {
   return thing
 }
 
-// This function check is player exist in the playing area
-// function checkIfPlayerInTheArea(field, x, y) {
-//   if (field[y] === undefined || field[y][x] === undefined) {
-//     console.log(
-//       'Looks like you got out of playing area!\nThe game`ll be restarted',
-//     )
-//     return process.exit()
-//   }
-// }
-// This function checks is snake's head went on top of it's body or it isn't
-function isPlayerStillAlive(snake, step) {
-  for (const { x, y } of snake) {
-    if (x === step.x && y === step.y) {
-      console.log(
-        'You just bited yourself! I guess apples aren\'t that good huh ?',
-      )
-      process.exit()
+function showCollecteables(field, collectables) {
+  const things = {
+    apple: chalk.red('@'),
+    resize: chalk.blue('⇆'),
+  }
+  const entries = Object.entries(collectables)
+  for (const [name, coordinates] of entries) {
+    field[coordinates.y][coordinates.x] = things[name]
+  }
+  return field
+}
+
+function playerEating(step, collectables) {
+  const entries = Object.entries(collectables)
+  for (const [name, coords] of entries) {
+    if (step.x === coords.x && step.y === coords.y) {
+      switch (name) {
+        case 'apple':
+          snake.push(step)
+          collectables.apple = generateThing(snake)
+          break
+        case 'resize':
+          width += 1
+          height += 1
+          collectables.resize = generateThing(snake)
+          break
+        default:
+          break
+      }
     }
   }
 }
-// TEST FUNCTION, CHECK IF PLAYER IS IN THE AREA
-function checkIfPlayerInTheArea(step, field) {
-  const { x, y } = step
-  if (field[y] === undefined || field[y][x] === undefined) {
-    console.log(
-      'Looks like you got out of playing area!\nThe game`ll be restarted',
-    )
-    return process.exit()
+
+function playerLengthProgress(snake) {
+  const targetScore = 50
+  const snakeLength = snake.length - 3
+  if(snakeLength % 10 === 0 ){
+    console.log(`${targetScore - snakeLength} Apples left! Keep going!`)
+  }
+  else if(snakeLength === targetScore){
+    console.log(`You ate all ${targetScore} apples. Congratulation!`)
+  }
+  else {
+    console.log(`You ate: ${snakeLength} apples.`)
   }
 }
 
-setInterval(makeAStep, 400)
+setInterval(makeAStep, 350)
 
 export default () => {
   return makeAStep()
